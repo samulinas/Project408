ddd3541dc2d23abd22a3e069b65baddc54f3b238
Stanislav Samulinas <samulinas@gmail.com>
2024-01-23 13:22:18 +0200

    lesson 23.01

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Привіт світ!");
+
 //Типи даних
-let param1;
-let param2 = [1, "Hello", true];
+let param1, param2 = [1, "Hello", true];
 let param3 = 35;
 let param4 = true;
 let param5 = "11" + param3;
@@ -10,6 +10,7 @@ let param7 = function hello() { console.log("Hello!")};
 let param8 = Symbol("id");
 console.log(typeof param1, typeof param2, typeof param3, typeof param4, typeof param5, typeof param6, typeof param7, typeof param8);
 console.log(param2, param5);
+
 //Операції
 let op1 = param3 + 20;
 let op2 = param3 - 20;
@@ -27,8 +28,34 @@ let bool6 = op1 !== op2;
 let bool7 = op1 == op2;
 let bool8 = op1 === op2;
 
-let bool9 = boolean(op1) && boolean(op2);
-let bool10 = op1 || op2;
+let bool9 = Boolean(op1 && !op2);
+let bool10 = op1 || !op2;
 
 console.log(op1, op2, op3, op4, op5, op6, bool1, bool2, bool3, bool4, bool5, bool6, bool7, bool8);
 console.log(bool9, bool10);
+
+//Взаємодія: alert, confirm, prompt
+//alert('Привіт, світ!');
+//const const1 = Math.PI;
+//console.log(const1);
+//let result = prompt("Cкільки вам років?", 18);
+//let result2 = confirm("Ви підтверджуєте замовлення?");
+//console.log(result, result2);
+
+// Операції присвоювання
+console.log(op1, op2, op3, op4, op5, op6);
+op1 += 10;
+op2 -= 10;
+op3 *= 10;
+op4 /= 10;
+op5 %= 10; // op5 = op5 % 10;
+op6 **= 10; // op6 = op6 ** 10;
+console.log(op1, op2, op3, op4, op5, op6);
+
+//Інкремент і декремент
+let a = 10;
+console.log(a++);
+console.log(a);
+console.log(a--);
+console.log(a);
+
