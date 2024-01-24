87025c36dcb27701c9e2c11ddf372b9f01208976
Stanislav Samulinas <samulinas@gmail.com>
2024-01-24 15:39:11 +0200

    lesson 24.01

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+"use strict"
 console.log("Привіт світ!");
 
 //Типи даних
-let param1, param2 = [1, "Hello", true];
+let param1,
+  param2 = [1, "Hello", true];
 let param3 = 35;
 let param4 = true;
 let param5 = "11" + param3;
 let param6 = 78687565353229887976n;
-let param7 = function hello() { console.log("Hello!")};
+let param7 = function hello() {
+  console.log("Hello!");
+};
 let param8 = Symbol("id");
-console.log(typeof param1, typeof param2, typeof param3, typeof param4, typeof param5, typeof param6, typeof param7, typeof param8);
+console.log(
+  typeof param1,
+  typeof param2,
+  typeof param3,
+  typeof param4,
+  typeof param5,
+  typeof param6,
+  typeof param7,
+  typeof param8
+);
 console.log(param2, param5);
 
 //Операції
@@ -31,7 +44,22 @@ let bool8 = op1 === op2;
 let bool9 = Boolean(op1 && !op2);
 let bool10 = op1 || !op2;
 
-console.log(op1, op2, op3, op4, op5, op6, bool1, bool2, bool3, bool4, bool5, bool6, bool7, bool8);
+console.log(
+  op1,
+  op2,
+  op3,
+  op4,
+  op5,
+  op6,
+  bool1,
+  bool2,
+  bool3,
+  bool4,
+  bool5,
+  bool6,
+  bool7,
+  bool8
+);
 console.log(bool9, bool10);
 
 //Взаємодія: alert, confirm, prompt
@@ -59,3 +87,51 @@ console.log(a);
 console.log(a--);
 console.log(a);
 
+//Умовні конструкції
+//let year = prompt("Який зараз рік?", 2000);
+/* if (year == 2024) {
+    alert( "Правильно!" );
+}
+else {
+    alert( "Неправильно!" );
+} */
+//(year == 2024) ?  alert( "Правильно!" ) : alert( "Неправильно!" );
+
+let user = null;
+//console.log(user ?? "Анонім");
+user ??= "Анонім"; // user = user ?? "Анонім"
+console.log(user);
+
+function calcSwitch() {
+  let b = parseInt(prompt("скільки буде 2+2?", 4));
+
+  switch (b) {
+    case 3:
+      alert("Малувато");
+      break;
+    case 4:
+      alert("В точку!");
+      break;
+    case 5:
+      alert("Перебір");
+      break;
+    default:
+      alert("Нема таких значень");
+      break;
+  }
+}
+let b = 5;
+console.log(b);
+
+
+
+let showMessage = function(hello = "Добрий день") {
+  console.log(hello);
+  return hello;
+}
+showMessage("Привіт!");
+showMessage("Вітаю");
+let c = showMessage();
+console.log(c);
+
+//calcSwitch();
